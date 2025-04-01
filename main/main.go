e15ca1439aeedfa8cd95dda1d1a065cea444a679
Vishal_2029 <Vishal.9dot@gmail.com>
2025-04-01 14:48:37 +0530

    Ignored CSV files

--- main/main.go
+++ main/main.go
@@ -35,6 +35,9 @@ func getMySQL() *sql.DB {
 }
 
 func main() {
+
+	startTime := time.Now()
+	fmt.Printf("Start Time: %s\n", startTime.Format(time.TimeOnly))
 	db := getMySQL()
 	if db == nil {
 		return
@@ -53,50 +56,59 @@ func main() {
 	file, err := os.Open("D:\\go\\src\\.go\\csv-databas-1\\top10milliondomains.csv")
 	if err != nil {
 		log.Fatal(err)
-	}  
+	}
 	df := csv.NewReader(file)
 	data, err := df.ReadAll()
 	if err != nil {
 		log.Fatal("Failed to read CSV file:", err)
 	}
-	 
-	
-	for _, files := range data {
-		fmt.Printf("Processing file: %s\n", files)
-	}
 
+	limit := 10000
+	count := 0
+	batch := 100
 	for i, row := range data {
-		if i == 0 { 
-			continue 
+		if i == 0 {
+			continue
 		}
-	
+
+		fmt.Printf("adding file: %s\n", row)
+
 		if len(row) < 3 {
 			log.Println("Skipping malformed row:", row)
 			continue
 		}
-	
+
 		Number, err := strconv.Atoi(strings.TrimSpace(row[0]))
 		if err != nil {
 			log.Println("Failed to parse Number:", row[0], err)
 			continue
 		}
-	
+
 		PageNumber, err := strconv.ParseFloat(strings.TrimSpace(row[2]), 64)
 		if err != nil {
 			log.Println("Failed to parse PageNumber:", row[2], err)
 			continue
 		}
-	
+
 		_, err = db.Exec(`INSERT INTO tenmil (Number, Domain, PageNumber) VALUES (?, ?, ?)`, Number, row[1], PageNumber)
 		if err != nil {
 			log.Println("Failed to insert record:", err)
 		}
+
+		count++
+		if count >= limit {
+			fmt.Println("Reached limit of 1000000 records. Stopping insertion.")
+			break 
+		}
 	}
 
-	fmt.Println("Finished processing")
+	fmt.Println("Finished processing", )
+	fmt.Printf("Start Time: %s\n", startTime.Format(time.TimeOnly))
 
-	startTime := time.Now()
-	fmt.Printf("Start Time: %s\n", startTime.Format(time.RFC3339))
+	endtime := time.Now()
+
+	executionTime := endtime.Sub(startTime)
+	fmt.Printf("execytion Time: %s\n", &executionTime)
+
 	fmt.Println("All Data Inserted Successfully!")
-	
 }
