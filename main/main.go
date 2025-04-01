83536f0f8f5a556c4ce6d04765d706cc3f0474b1
Vishal-2029 <vishal.9dot@gmail.com>
2025-04-01 14:44:02 +0530

    main.go

--- main/main.go
+++ main/main.go
@@ -6,97 +6,39 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strconv"
-	"strings"
-	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
-type Data struct {
-	Number     int
-	Domain     string
-	PageNumber float64
+type Vehicle struct {
+	Vehicle string
 }
 
 func getMySQL() *sql.DB {
-	db, err := sql.Open("mysql", "root:root@(127.0.0.1:3306)/10mil_db?parseTime=true")
+	db, err := sql.Open("mysql", "root:root@(127.0.0.1:3306)/GTA_data_db?parseTime=true")
 	if err != nil {
-		log.Fatal("Failed to connect to database:", err)
-		return nil
+		log.Fatal(err)
 	}
-
-	if err = db.Ping(); err != nil {
-		log.Fatal("Failed to ping database:", err)
-		return nil
-	}
-
 	return db
 }
 
 func main() {
 	db := getMySQL()
-	if db == nil {
-		return
-	}
-	defer db.Close()
 
-	_, err := db.Exec(`CREATE TABLE IF NOT EXISTS tenmil (
-		Number   INT,
-		Domain    VARCHAR(255),
-		pageNumber DECIMAL(10,2)
-	)`)
-	if err != nil {
-		log.Fatal("Failed to create table:", err)
-	}
-
-	file, err := os.Open("D:\\go\\src\\.go\\csv-databas-1\\top10milliondomains.csv")
+	file, err := os.Open("C:\\Users\\visha\\Downloads\\archive\\vehicle_links.csv")
 	if err != nil {
 		log.Fatal(err)
-	}  
+	}
 	df := csv.NewReader(file)
-	data, err := df.ReadAll()
-	if err != nil {
-		log.Fatal("Failed to read CSV file:", err)
+	data,_ := df.ReadAll()
+	var vehicles []Vehicle
+	for _, value := range data {
+		vehicles = append(vehicles, Vehicle{Vehicle: value[0]})
 	}
-	 
-	
-	for _, files := range data {
-		fmt.Printf("Processing file: %s\n", files)
-	}
-
-	for i, row := range data {
-		if i == 0 { 
-			continue 
-		}
-	
-		if len(row) < 3 {
-			log.Println("Skipping malformed row:", row)
-			continue
-		}
-	
-		Number, err := strconv.Atoi(strings.TrimSpace(row[0]))
-		if err != nil {
-			log.Println("Failed to parse Number:", row[0], err)
-			continue
-		}
-	
-		PageNumber, err := strconv.ParseFloat(strings.TrimSpace(row[2]), 64)
-		if err != nil {
-			log.Println("Failed to parse PageNumber:", row[2], err)
-			continue
-		}
-	
-		_, err = db.Exec(`INSERT INTO tenmil (Number, Domain, PageNumber) VALUES (?, ?, ?)`, Number, row[1], PageNumber)
-		if err != nil {
-			log.Println("Failed to insert record:", err)
-		}
+ 
+	for i := 1; i < len(vehicles); i++ { 
+		db.Exec("Create Table vehicle_links (vehicle) values (?)", vehicles[i].Vehicle)
 	}
 
-	fmt.Println("Finished processing")
-
-	startTime := time.Now()
-	fmt.Printf("Start Time: %s\n", startTime.Format(time.RFC3339))
-	fmt.Println("All Data Inserted Successfully!")
-	
+	fmt.Println("Data Inserted")
 }
